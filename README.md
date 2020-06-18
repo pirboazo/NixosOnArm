68e9e3d5d78cf2f336f77d6fbd943f21fbe8a945
pirboazo <pib@pirboazo.net>
2020-06-18 16:04:22 +0200

    nouvelle configuration

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # NixosOnArm
-Mon dépôt pour gérer mes fichiers de configuration
+Mon dépôt pour gérer mes fichiers de configuration des instalation Nixos on ARM aarch64 sur Rpi3B.
 
-Vous trouverez deux répertoires 
+Vous trouverez ici trois répertoires où vous trouverez les fichiers de configuration.nix valides pour installer Nixos sur un rpi 3B comme décrit dans cet [article](https://boizot.ch/#!nixos/OnArmAarch64.md)
 
-* minimal le fichier configuration.nix valide pour installer Nixos sur un rpi 3B comme décrit dans cet [article](https://boizot.ch/#!nixos/OnArmAarch64.md) 
-* xserver : exploration encours 
-
+* minimal : avec  le fichier configuration.nix  avec ou sans zramswap & documentation
+* xserver : le fichier de configuration valide pour installer les librairies X11 mais n'activant pas le Xserver ** version lastPackages ** 
+* mate : le fichier de configuration valide pour installer & activer le Xserver ** version Kernel 4_19 **
 
--- mate/configuration.nix
+++ mate/configuration.nix
@@ -8,12 +8,17 @@
   boot.loader.generic-extlinux-compatible.enable = true;
 #
 # if you have a Raspberry Pi 2 or 3), pick this:
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_4_19;
   nixpkgs.config.allowUnsupportedSystem = true; 
 
 # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
   # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
-  boot.kernelParams = lib.mkForce [ "cma=64M" ];
+  boot.kernelParams = lib.mkForce [
+     "console=ttyS1,115200n8"
+     "console=tty1"
+     "loglevel=7"
+     "cma=256M"
+  ];
 
   networking.hostName = "rpi3-Nixos"; # Define your hostname.
 
@@ -41,16 +46,14 @@
 
  # Enable the X11 windowing system.
    services.xserver.enable = true;
-   services.xserver.autorun = false;
    services.xserver.layout = "ch";
    services.xserver.xkbVariant = "fr";
    services.xserver.xkbOptions = "eurosign:e";
+   services.xserver.videoDrivers = [ "fbdev" ] ;
 
   # Enable the gnome3 Desktop Environment.
-   # services.xserver.displayManager.sddm.enable = true;
-   # services.xserver.desktopManager.mate.enable = true;
-#
-   services.xserver.videoDrivers = [ "modesetting" ] ;
+   services.xserver.displayManager.sddm.enable = true;
+   services.xserver.desktopManager.mate.enable = true;
 
 # enable zramswap
 zramSwap = {
@@ -71,5 +74,5 @@ zramSwap = {
     description = "pierre test";
     extraGroups = [ "wheel" "networkmanager" ];
   };
-  environment.systemPackages = with pkgs; [ wget bat vim lshw pciutils banner usbutils htop tmux ];
+  environment.systemPackages = with pkgs; [ wget bat vim lshw pciutils banner usbutils midori htop tmux ];
 }
